d1000b2328bf75f9c68603372cb60e80ad906aba
Yinka Adedire <dryinkuzz@gmail.com>
2020-08-22 23:03:41 +0100

    fix vertical overflow on mobile

--- src/app.js
+++ src/app.js
@@ -2,11 +2,15 @@ import React, { useState, useEffect, useRef, Fragment } from 'react';
 import styled, { css, createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-* {
+  * {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
   outline: none;
+}
+
+  html {
+  height: 100%;
 }
 
   body {
@@ -14,7 +18,10 @@ const GlobalStyle = createGlobalStyle`
     opacity: 0.9;
     margin: 0;
     padding: 0;
+    min-height: 100%;
     box-sizing: border-box;
+    display: grid;
+    place-items: center;
     font-family: sans-serif;
     background-color: #ff7700;
     background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1600 900'%3E%3Cpolygon fill='%23cc0000' points='957 450 539 900 1396 900'/%3E%3Cpolygon fill='%23aa0000' points='957 450 872.9 900 1396 900'/%3E%3Cpolygon fill='%23d6002b' points='-60 900 398 662 816 900'/%3E%3Cpolygon fill='%23b10022' points='337 900 398 662 816 900'/%3E%3Cpolygon fill='%23d9004b' points='1203 546 1552 900 876 900'/%3E%3Cpolygon fill='%23b2003d' points='1203 546 1552 900 1162 900'/%3E%3Cpolygon fill='%23d3006c' points='641 695 886 900 367 900'/%3E%3Cpolygon fill='%23ac0057' points='587 900 641 695 886 900'/%3E%3Cpolygon fill='%23c4008c' points='1710 900 1401 632 1096 900'/%3E%3Cpolygon fill='%239e0071' points='1710 900 1401 632 1365 900'/%3E%3Cpolygon fill='%23aa00aa' points='1210 900 971 687 725 900'/%3E%3Cpolygon fill='%23880088' points='943 900 1210 900 971 687'/%3E%3C/svg%3E");
@@ -53,7 +60,6 @@ const StopwatchWrapper = styled.div`
   width: '100%';
   display: flex;
   flex-direction: column;
-  min-height: 100vh;
   align-items: center;
   justify-content: center;
 `;
