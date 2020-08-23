a04f1088c392e1e799b12e92cadd3671ada787fb
Yinka Adedire <dryinkuzz@gmail.com>
2020-08-23 15:19:53 +0100

    add and style footer

--- src/app.js
+++ src/app.js
@@ -13,15 +13,18 @@ const GlobalStyle = createGlobalStyle`
   height: 100%;
 }
 
+#root {
+  height: 100%;
+}
+
   body {
     color: #ffffff;
     opacity: 0.9;
+    height:100%;
     margin: 0;
     padding: 0;
     min-height: 100%;
     box-sizing: border-box;
-    display: grid;
-    place-items: center;
     font-family: sans-serif;
     background-color: #ff7700;
     background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1600 900'%3E%3Cpolygon fill='%23cc0000' points='957 450 539 900 1396 900'/%3E%3Cpolygon fill='%23aa0000' points='957 450 872.9 900 1396 900'/%3E%3Cpolygon fill='%23d6002b' points='-60 900 398 662 816 900'/%3E%3Cpolygon fill='%23b10022' points='337 900 398 662 816 900'/%3E%3Cpolygon fill='%23d9004b' points='1203 546 1552 900 876 900'/%3E%3Cpolygon fill='%23b2003d' points='1203 546 1552 900 1162 900'/%3E%3Cpolygon fill='%23d3006c' points='641 695 886 900 367 900'/%3E%3Cpolygon fill='%23ac0057' points='587 900 641 695 886 900'/%3E%3Cpolygon fill='%23c4008c' points='1710 900 1401 632 1096 900'/%3E%3Cpolygon fill='%239e0071' points='1710 900 1401 632 1365 900'/%3E%3Cpolygon fill='%23aa00aa' points='1210 900 971 687 725 900'/%3E%3Cpolygon fill='%23880088' points='943 900 1210 900 971 687'/%3E%3C/svg%3E");
@@ -57,7 +60,7 @@ const StyledButton = styled.button`
 `;
 
 const StopwatchWrapper = styled.div`
-  width: '100%';
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -179,11 +182,66 @@ const Stopwatch = () => {
   );
 };
 
+const StyledFooter = styled.footer`
+  padding: 1rem;
+  font-size: 1rem;
+  background: hsla(0, 0%, 0%, 0.2);
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-around;
+
+  & a:hover {
+    color: #24f5b9;
+  }
+
+  & a {
+    text-decoration: underline;
+    color: inherit;
+  }
+`;
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+      <p>
+        Designed and Built by
+        <a href="https://twitter.com/yinkakun" target="blank">
+          Yinka Adedire
+        </a>
+      </p>
+      <p>
+        <a href="https://github.com/yinkakun/react-stopwatch" target="blank">
+          Star on Github
+        </a>
+      </p>
+    </StyledFooter>
+  );
+};
+
+const LayoutWrapper = styled.div`
+  display: grid;
+  grid-template-rows: 1fr auto;
+  width: 100%;
+  height: 100%;
+`;
+
+const Layout = ({ children }) => {
+  return (
+    <LayoutWrapper>
+      {children}
+      <Footer />
+    </LayoutWrapper>
+  );
+};
+
 const App = () => {
   return (
     <Fragment>
       <GlobalStyle />
-      <Stopwatch />
+      <Layout>
+        <Stopwatch />
+      </Layout>
     </Fragment>
   );
 };
